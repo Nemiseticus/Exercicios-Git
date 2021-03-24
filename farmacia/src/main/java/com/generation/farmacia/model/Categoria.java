347b3d61c39833f95def70fa28f976c0ce524527
Nemiseticus <Nemiseticus>
2021-03-23 21:24:48 -0300

    farmacia

---
+++ farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,70 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	private String nome;
+	
+	@NotNull
+	private String classificao;
+	
+	@OneToMany(mappedBy = "categoriaDoProduto")
+	@JsonIgnoreProperties ("categoriaDoproduto")
+	private List<Produto> produtos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getClassificao() {
+		return classificao;
+	}
+
+	public void setClassificao(String classificao) {
+		this.classificao = classificao;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+
+	
+	
+}
